274eea57891a24e32a45dd480a724af7637bafff
Andy Low <andyjunchenglow@gmail.com>
2022-11-14 01:25:57 +1100

    Completed bday card section, optimized for pc view

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function displayAdeline() {
   // Insert your full name here
   document.getElementById("summary-title").innerHTML = "ADELINE";
 
-  // Insert your details here
+  // Insert stats here
   document.getElementById("summary-text").innerHTML =
     '"What do you call poop that looks like turtles?"' +
     "<br>" +
@@ -35,10 +35,33 @@ function displayAdeline() {
     "<br>" +
     "Sometime in July 2022." +
     "<br>" +
-    "After the Hareruya day we studied tgt and I installed the Anya Cursor, we went to Brunetti's with Calvin and talked about Javier."
+    "After the Hareruya day we studied tgt and I installed the Anya Cursor, we went to Brunetti's with Calvin and talked about Mr. J."
+
+    // Insert stats here
+    document.getElementById("bday-card-content").innerHTML = 
+    "Now for the actual birthday card ..." +
+    "<br>" +
+    "<br>" +
+    "<br>" +
+    "Hey Ad, congrats on becoming a young adult, you're no more a teen but hey, at least you're one step closer to becoming a doctor, a.k.a. med cert supplier." +
+    "<br>" +
+    "<br>" +
+    "I will miss all your dry jokes and all the times our minds would make fun of the same stuff before we even say it. Can't believe I've descended to your level of humour :P. " +
+    "Hopefully, we'll get to meet every so often, but probs not as often as before *thank god*" +
+    "<br>" +
+    "<br>" +
+    "Anywayyyys, jokes aside, I am glad to have met you, wish it could've been earlier. May your journey in life be as smooth as your turdles too. Happy Birthday!!!" +
+    "<br>" +
+    "<br>" +
+    "Oh yeah, hope you like the Kirby too, was gonna make a knife for it, but ceebs." +
+    "<br>" +
+    "<br>" +
+    "P.S. Yes, I did make this website green on purpose."
 
   // Select your sprite by changing the number
   document.getElementById("sprite").src = "./res/sprite-ad.png";
+  var y = document.getElementById("bday-card-content");
+  y.style.padding = "40px 60px 70px 60px";
 }
 
 function changeBlue() {
@@ -90,7 +113,7 @@ function selected() {
   body.style.borderRight = "solid 4px var(--black)";
   body.style.paddingTop = "90px";
 
-  const hidden = document.getElementById("hidden");
+  const hidden = document.getElementById("bday-card-content");
   hidden.style.borderRight = "solid 4px var(--black)";
 
   }
@@ -106,4 +129,8 @@ function selected() {
   // Displays the select button
   const selectCharacter = document.getElementById("characterSelect");
   selectCharacter.style.display = "block";
+}
+
+function goToPage() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
